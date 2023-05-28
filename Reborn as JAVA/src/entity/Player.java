f40a91f399029f2e8335f55cab5fc2f19bc9edda
Randold8 <84187343+Randold8@users.noreply.github.com>
2023-05-29 02:53:24 +0300

    anim

--- Reborn as JAVA/src/entity/Player.java
+++ Reborn as JAVA/src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -14,36 +17,64 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
     public void update() {
-        /* if(keyH.upPressed == true) {
-            y-= speed;
+        if(keyH.upPressed == 1) {
+            direction = "up";
         }
-        else if (keyH.downPressed == true) {
-            y += speed;
+        else if (keyH.downPressed == 1) {
+            direction = "down";
         }
-        else if(keyH.leftPressed == true) {
-            x -= speed;
+        else if(keyH.leftPressed == 1) {
+            direction = "left";
         }
-        else if(keyH.rightPressed == true) {
-            x += speed;
+        else if(keyH.rightPressed == 1) {
+            direction = "right";
         }
-        */
+
         x = x + speed*keyH.rightPressed - speed*keyH.leftPressed;
         y = y + speed*keyH.downPressed - speed*keyH.upPressed;
 
     }
     public void draw( Graphics2D g2) {
-        g2.setColor(Color.white);
-
-        g2.fillRect(x, y,gp.tileSize,gp.tileSize);
-
-        g2.dispose();
+        BufferedImage image = null;
+        switch(direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "right":
+                image = right1;
+                break;
+            case "left":
+                image = left1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
 }
