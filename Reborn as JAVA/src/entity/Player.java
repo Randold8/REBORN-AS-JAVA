8df9ff893466219390b56a88d082e2daf58a0701
Randold8 <84187343+Randold8@users.noreply.github.com>
2023-05-29 03:00:22 +0300

    anim2

--- Reborn as JAVA/src/entity/Player.java
+++ Reborn as JAVA/src/entity/Player.java
@@ -57,21 +57,52 @@ public class Player extends Entity {
         x = x + speed*keyH.rightPressed - speed*keyH.leftPressed;
         y = y + speed*keyH.downPressed - speed*keyH.upPressed;
 
+        spriteCounter++;
+        if (spriteCounter > 10) {
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+
     }
     public void draw( Graphics2D g2) {
         BufferedImage image = null;
         switch(direction) {
             case "up":
-                image = up1;
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
         }
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
