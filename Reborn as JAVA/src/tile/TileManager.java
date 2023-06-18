0dfcf56213ea86efaed4cb2fa3659c3c9ac0cc7d
Randold8 <84187343+Randold8@users.noreply.github.com>
2023-06-18 23:34:19 +0300

    fixed camera
    cool

--- Reborn as JAVA/src/tile/TileManager.java
+++ Reborn as JAVA/src/tile/TileManager.java
@@ -168,7 +168,7 @@ public class TileManager {
             }
             worldCol++;
 
-            if(worldCol == gp.maxScreenCol) {
+            if(worldCol == gp.maxWorldCol) {
                 worldCol = 0;
                 worldRow++;
             }
